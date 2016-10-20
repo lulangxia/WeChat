2e82548ad64ff568d39d2f37ec52241c419b2307
zhangjinglin <627586618@qq.com>
2016-10-20 21:58:24 +0800

    toolbar功能

--- app/src/main/java/com/zjl/mywechat/MainActivity.java
+++ app/src/main/java/com/zjl/mywechat/MainActivity.java
@@ -1,17 +1,22 @@
 package com.zjl.mywechat;
 
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends BaseAty {
+public class MainActivity extends BaseAty implements Toolbar.OnMenuItemClickListener {
 
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private Toolbar mToolbar;
 
     @Override
     protected int setLayout() {
@@ -22,21 +27,44 @@ public class MainActivity extends BaseAty {
     protected void initView() {
         mTabLayout = bindView(R.id.titles_main);
         mViewPager = bindView(R.id.fragments_main);
+        mToolbar = bindView(R.id.toolbar);
 
     }
 
     @Override
     protected void initData() {
+        mToolbar.setTitle("微信");
+        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+        mToolbar.setOnMenuItemClickListener(this);
+
+
         ArrayList<Fragment> fragments = new ArrayList<>();
         fragments.add(new TestFragment());
         fragments.add(new TestFragment());
         fragments.add(new TestFragment());
         fragments.add(new TestFragment());
 
-        MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager(),this);
+        MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager(), this);
         mainAdapter.setFragments(fragments);
         mViewPager.setAdapter(mainAdapter);
         mTabLayout.setTabMode(TabLayout.MODE_FIXED);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Toast.makeText(MainActivity.this, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_add:
+                Toast.makeText(MainActivity.this, "添加", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
     }
 }
