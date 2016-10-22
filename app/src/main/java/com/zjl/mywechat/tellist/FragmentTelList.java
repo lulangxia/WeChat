2b534ccdcf88388ad1df797ea208f4a7fc9ef791
zhangjinglin <627586618@qq.com>
2016-10-22 17:27:16 +0800

    界面完善

--- app/src/main/java/com/zjl/mywechat/tellist/FragmentTelList.java
+++ app/src/main/java/com/zjl/mywechat/tellist/FragmentTelList.java
@@ -2,6 +2,8 @@ package com.zjl.mywechat.tellist;
 
 
 import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,6 @@ import java.util.HashMap;
 import java.util.TreeSet;
 
 
-
 public class FragmentTelList extends BaseFragment {
 
     private ListView lvPeople;
@@ -30,15 +31,13 @@ public class FragmentTelList extends BaseFragment {
     private int height;
 
     // 字典对应
-    private String[] indexStr = {"☆", "↑", "A", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"};
+    private String[] indexStr = {"↑", "☆", "A", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z", "#"};
     // 没排序的
     private ArrayList<TelListBean> telListBeen;
     // 排好序的？/ 姓氏的汉字排序？
     private ArrayList<TelListBean> newTelListBeen;
     // 索引
     private HashMap<String, Integer> selector;
-
-
 
 
     @Override
@@ -48,15 +47,19 @@ public class FragmentTelList extends BaseFragment {
 
     @Override
     protected void initView() {
+        Log.d("FragmentTelList", "view");
+
         lvPeople = bindView(R.id.lv_main_tellist);
         tvCharacter = bindView(R.id.tv_tellist_character);
         llCharacters = bindView(R.id.ll_main_tellist);
-        tvCharacter.setVisibility(View.INVISIBLE);
+        tvCharacter.setVisibility(View.GONE);
     }
 
     @Override
     protected void initData() {
-
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.contract_fragment, null);
+        lvPeople.addHeaderView(view);
+        Log.d("FragmentTelList", "data");
         telListBeen = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             TelListBean bean = new TelListBean("张三" + i, "123");
@@ -87,11 +90,7 @@ public class FragmentTelList extends BaseFragment {
                 return true;
             }
         });
-
-
     }
-
-
 
 
     private void initSendInternet() {
@@ -116,8 +115,6 @@ public class FragmentTelList extends BaseFragment {
 
 
     }
-
-
 
 
     // 根据索引排序（封装好了的）
@@ -147,9 +144,6 @@ public class FragmentTelList extends BaseFragment {
     }
 
 
-
-
-
     // ListView内容的排序
     private void sortList(String[] allNames) {
         for (int i = 0; i < allNames.length; i++) {
@@ -165,11 +159,6 @@ public class FragmentTelList extends BaseFragment {
             }
         }
     }
-
-
-
-
-
 
 
     // 根据索引拿到ListView的东西
@@ -192,6 +181,11 @@ public class FragmentTelList extends BaseFragment {
                         String key = indexStr[index];
                         // selector没数据、、、
                         // 需要把数据索引添加到那里面去！
+                        if (index == 0) {
+                            lvPeople.setSelectionFromTop(0, 0);
+                            tvCharacter.setVisibility(View.VISIBLE);
+                            tvCharacter.setText(indexStr[index]);
+                        }
                         if (selector.containsKey(key)) {
                             int pos = selector.get(key);
                             if (lvPeople.getHeaderViewsCount() > 0) {
@@ -205,10 +199,10 @@ public class FragmentTelList extends BaseFragment {
                     }
                     switch (event.getAction()) {
                         case MotionEvent.ACTION_DOWN:
-                            llCharacters.setBackgroundColor(Color.parseColor("#00ffffff"));
+                            llCharacters.setBackgroundColor(0x5f9a9a9a);
                             break;
                         case MotionEvent.ACTION_UP:
-                            llCharacters.setBackgroundColor(Color.parseColor("#00ffffff"));
+                            llCharacters.setBackgroundColor(Color.parseColor("#00000000"));
                             tvCharacter.setVisibility(View.INVISIBLE);
                             break;
                         case MotionEvent.ACTION_MOVE:
@@ -221,6 +215,14 @@ public class FragmentTelList extends BaseFragment {
 
         }
 
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        flag = false;
+        Log.d("FragmentTelList", "res");
     }
 
 
