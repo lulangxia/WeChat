781d14ebcb8619f7f45b0c039c0c68ae49ce2fb9
dllo <dllo@dllodeMac-mini-45.local>
2016-10-22 14:12:46 +0800

    这个是新增注册页面

---
+++ app/src/main/java/com/zjl/mywechat/login_register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.zjl.mywechat.login_register;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zjl.mywechat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
