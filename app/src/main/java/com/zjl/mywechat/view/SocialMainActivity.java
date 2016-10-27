5a5679ea703aed5219893df1bdac1e4b7083b899
胡帅 <1174823854@qq.com>
2016-10-27 14:35:27 +0800

    fragmentfind找到控件和添加点击事件

---
+++ app/src/main/java/com/zjl/mywechat/view/SocialMainActivity.java
@@ -0,0 +1,25 @@
+package com.zjl.mywechat.view;
+
+import com.zjl.mywechat.R;
+import com.zjl.mywechat.base.BaseAty;
+
+
+
+public class SocialMainActivity extends BaseAty {
+
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_social_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
