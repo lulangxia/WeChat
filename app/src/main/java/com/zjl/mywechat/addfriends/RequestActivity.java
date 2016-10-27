0435e9904a2766f8975e25673c0450e275f46473
刘雨辰 <675874375@qq.com>
2016-10-27 17:28:08 +0800

    添加好友与接收别人的请求

---
+++ app/src/main/java/com/zjl/mywechat/addfriends/RequestActivity.java
@@ -0,0 +1,121 @@
+package com.zjl.mywechat.addfriends;
+
+
+import android.graphics.Color;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+
+import com.hyphenate.EMContactListener;
+import com.hyphenate.chat.EMClient;
+import com.zjl.mywechat.R;
+import com.zjl.mywechat.app.MyApp;
+import com.zjl.mywechat.base.BaseAty;
+import com.zjl.mywechat.base.BaseListViewAdapter;
+import com.zjl.mywechat.base.BaseListViewHolder;
+
+import java.util.ArrayList;
+
+public class RequestActivity extends BaseAty {
+
+
+    private ListView lv;
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_request;
+    }
+
+    @Override
+    protected void initView() {
+
+        Toolbar toolbar = bindView(R.id.toolbar_request);
+        toolbar.setNavigationIcon(R.mipmap.fx_icon_back_n);
+        toolbar.setTitle("添加朋友");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        lv = bindView(R.id.lv_request);
+
+
+
+
+
+
+        EMClient.getInstance().contactManager().setContactListener(new EMContactListener() {
+            @Override
+            public void onContactAdded(String s) {
+                Log.d("RequestActivity", "加好友");
+            }
+
+            @Override
+            public void onContactDeleted(String s) {
+                Log.d("RequestActivity", "加好友");
+            }
+
+            @Override
+            public void onContactInvited(String s, String s1) {
+                Log.d("RequestActivity", "加好友");
+            }
+
+            @Override
+            public void onContactAgreed(String s) {
+                // 同意了请求？
+                Log.d("RequestActivity", "加好友");
+            }
+
+            @Override
+            public void onContactRefused(String s) {
+                // 被拒绝了请求？
+                Log.d("RequestActivity", "加好友");
+            }
+        });
+
+
+
+        // 里面的数据要从网络端获取
+        ArrayList<RequestBean> been = new ArrayList<>();
+
+
+        lv.setAdapter(new BaseListViewAdapter<RequestBean>(MyApp.getmContext(), been, R.layout.item_personrequest) {
+            @Override
+            public void convent(BaseListViewHolder viewHolder, RequestBean requestBean) {
+                viewHolder.setText(R.id.tv_add_personname, requestBean.getName());
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.request, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+
+    @Override
+    protected void initData() {
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/zjl/mywechat/addfriends/RequestBean.java
@@ -0,0 +1,25 @@
+package com.zjl.mywechat.addfriends;
+
+
+public class RequestBean {
+
+    private String name;
+    private String img;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
